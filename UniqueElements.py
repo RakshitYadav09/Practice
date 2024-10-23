c0926f9169a0f7526df040dc13322c7127218657
Rakshit <rakshityadav411@gmail.com>
2024-10-23 22:44:02 +0530

    using sorting

--- UniqueElements.py
+++ UniqueElements.py
@@ -1,15 +1,26 @@
 def findDistinct(arr):
     res = []
     
-    for i in range (len(arr)):
+    # Nested loops
+    # for i in range (len(arr)):
         
-        j=0
-        while j<i: 
-            if arr[i]==arr[j]:
-                break
-            j += 1
-        if i==j:
+    #     j=0
+    #     while j<i: 
+    #         if arr[i]==arr[j]:
+    #             break
+    #         j += 1
+    #     if i==j:
+    #         res.append(arr[i])
+    # return res
+    
+    # Using sorting
+    n=len(arr)
+    arr.sort()
+    
+    for i in range(n):
+        if i == 0 or arr[i] != arr[i-1]:
             res.append(arr[i])
+            
     return res
 
 if __name__ == "__main__":
