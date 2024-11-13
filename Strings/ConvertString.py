b953145b3b88cb12e05703cd80c342e1e8db3e1d
Rakshit <rakshityadav411@gmail.com>
2024-11-13 21:32:38 +0530

    less time and space

--- Strings/ConvertString.py
+++ Strings/ConvertString.py
@@ -1,12 +1,25 @@
-def Convert(string):
-    while True:
-        i = 0
-        while i < len(string):
-            if string[i] == ' ':
-                string = string[:i] + string[i+1:].capitalize()
-                break
-            i += 1 
-        if i == len(string):
-            break
-    return string
+# def Convert(string):
+#     while True:
+#         i = 0
+#         while i < len(string):
+#             if string[i] == ' ':
+#                 string = string[:i] + string[i+1:].capitalize()
+#                 break
+#             i += 1 
+#         if i == len(string):
+#             break
+#     return string
  
+def convert(s):
+    if(len(s) == 0):
+        return
+    s1 = ''
+    s1 += s[0].upper()
+    for i in range(1, len(s)):
+        if (s[i] == ' '):
+            s1 += s[i + 1].upper()
+            i += 1
+        elif(s[i - 1] != ' '):
+            s1 += s[i] 
+    print(s1)     
+    
